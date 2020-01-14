1d311077d6635b69f121d0c887ef82fc36c976c5
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2020-01-14 14:34:05 -0500

    working shifted binary search

--- day_one/shifted_search.js
+++ day_one/shifted_search.js
@@ -1,23 +1,31 @@
-let shifted = [19, 27, 28, 33, 39, 41, 46, 49, 2, 5, 6, 10, 13, 14, 15, 17] 
-let sorted = [2, 5, 6, 10, 13, 14, 15, 17, 19, 27, 28, 33, 39, 41, 46, 49 ]
-let target = 33
+let shifted = [33, 39, 41, 46, 49, 2, 5, 6, 10, 13, 14, 15, 17, 19, 27, 28];
+
+let shiftedTwo = [13, 14, 15, 17, 19, 27, 28, 33, 39, 41, 46, 49, 2, 5, 6, 10];
+
+let sorted = [2, 5, 6, 10, 13, 14, 15, 17, 19, 27, 28, 33, 39, 41, 46, 49];
 
 const binary_search = (arr, target) => {
-    let low = 0
-    let high = arr.length - 1
-    let firstVal = arr[0]
+    let low = 0;
+    let high = arr.length - 1;
 
-    while(low <= high){
-        let middle = Math.floor((low + high) / 2)
-        let testNum = arr[middle]
-        if(testNum === target)
-            return middle
-        else if(testNum > target && target > firstVal){
-            high = middle - 1
+    while (low <= high) {
+        let firstVal = arr[low];
+        let lastVal = arr[high];
+        let middleIndex = Math.floor((low + high) / 2);
+        let middleNum = arr[middleIndex];
+        if (middleNum === target) return middleIndex;
+        else if (target < middleNum) {
+            if (target <= lastVal && lastVal < middleNum) 
+                low = middleIndex + 1;
+            else 
+                high = middleIndex - 1;
+        } else if (target > middleNum) {
+            if (target >= firstVal && firstVal > middleNum)
+                high = middleIndex - 1;
+            else 
+                low = middleIndex + 1;
         }
-        else
-            low = middle + 1
     }
-}
+};
 
-console.log(binary_search(shifted, target))
+sorted.forEach(num => console.log(num, binary_search(shiftedTwo, num)));
