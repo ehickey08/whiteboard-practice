2cbbc0a13996160b244b5615d8e93a1aa9d4334c
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2020-01-17 13:18:56 -0500

    day 3 matrix search - naive approach

---
+++ day_three/matrixSearch.js
@@ -0,0 +1,26 @@
+let matrix = [
+    [1, 4, 7, 12, 15, 997, 999],
+    [2, 5, 19, 32, 35, 1001, 1007],
+    [4, 8, 24, 34, 36, 1008, 1015],
+    [40, 41, 42, 44, 45, 1018, 1020],
+    [98, 99, 101, 104, 190, 1021, 1025],
+];
+
+
+const matrixSearch = (matrix, target) => {
+    let width = matrix[0].length
+    let height = matrix.length
+    let row = 0
+    while(true){
+        for(let col = 0; col < width; col++){
+            let testNum = matrix[row][col]
+            if(testNum === target)
+                return [row, col]
+            if(testNum > target)
+                break
+        }
+        row++
+    }
+}
+
+console.log(matrixSearch(matrix, 98))
