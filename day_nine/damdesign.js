c0886d5891073421ed65a04be00e54d1a9b94004
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2020-02-17 16:25:57 -0500

    Update and fix dam design

--- day_nine/damdesign.js
+++ day_nine/damdesign.js
@@ -3,21 +3,34 @@ const wallHeights = [4, 6, 8, 11];
 
 const damDesign = (wallPos, wallHeights) => {
     let previous = wallPos[wallPos.length - 1];
+    let maxMud = 0;
     for (let i = wallPos.length - 2; i >= 0; i--) {
         let current = wallPos[i];
         if (current === previous - 1) {
             previous = current;
             continue;
         } else {
-            let maxMud = wallHeights[i];
-            for (let i = current + 1; i < previous; i++) {
-                maxMud++;
-            }
-            return maxMud;
+            let localMax = buildMud(current, previous, wallHeights, i);
+
+            if (localMax > maxMud) maxMud = localMax;
         }
     }
 
-    return 0;
+    return maxMud;
+};
+
+const buildMud = (current, previous, wallHeights,  i) => {
+    let localMax;
+    let prevHeight = wallHeights[i + 1];
+    let currHeight = wallHeights[i];
+    let totalGaps = previous - current;
+    let neededSteps = prevHeight - currHeight;
+
+    if (neededSteps > totalGaps) return 0;
+    else if (neededSteps === totalGaps) localMax = prevHeight - 1;
+    else localMax = Math.floor((totalGaps - neededSteps) / 2) + prevHeight;
+
+    return localMax
 };
 
 console.log(damDesign(wallPositions, wallHeights));
