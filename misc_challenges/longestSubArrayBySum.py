9abc303fdb5b7cd1dcb6ebb1d2de5a69f0960eca
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2020-03-18 19:08:57 -0400

    cleanup findsubarray

--- misc_challenges/longestSubArrayBySum.py
+++ misc_challenges/longestSubArrayBySum.py
@@ -1,26 +1,21 @@
 def findLongestSubarrayBySum(s, arr):
     window_sum = 0
     start_pointer = 0
+    best = [-1, -2]
 
     for i, num in enumerate(arr):
         window_sum += num
 
-        if window_sum > s:
-            while window_sum > s:
-                window_sum -= arr[start_pointer]
-                start_pointer += 1
+        while window_sum > s:
+            window_sum -= arr[start_pointer]
+            start_pointer += 1
 
         if window_sum == s:
-            end_pointer = i
-            end_pointer += 1
-            while end_pointer < len(arr) and arr[end_pointer] == 0:
-                end_pointer += 1
-            return [start_pointer + 1, end_pointer]
+            possible = [start_pointer + 1, i + 1]
+            if (possible[1] - possible[0]) > (best[1] - best[0]):
+                best = possible
 
-    return [-1]
-
-
-testArr = [28, 68, 142, 130, 41, 14, 175, 2, 78, 16, 84, 14, 193, 25, 2, 193, 160, 71, 29, 28, 85, 76, 187, 99, 171, 88, 48, 5,
-           104, 22, 64, 107, 164, 11, 172, 90, 41, 165, 143, 20, 114, 192, 105, 19, 33, 151, 6, 176, 140, 104, 23, 99, 48, 185, 49, 172, 65]
-
-print(findLongestSubarrayBySum(1562, testArr))
+    if best[0] != -1:
+        return best
+    else:
+        return [-1]
