3c6c9c741001ae1912b060d12c396e0aa67e4278
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2020-04-02 12:23:44 -0400

    Math, create number from leet code

---
+++ from_dustin/createNumber.js
@@ -0,0 +1,35 @@
+/* 
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+Example 1:
+Input: [10,2]
+Output: "210"
+
+Example 2:
+Input: [3,30,34,5,9]
+Output: "9534330"
+
+Note: The result may be very large, so you need to return a string instead of an integer.
+
+*/
+
+var largestNumber = function(nums) {
+    let noZeros = nums.filter(num => num !== 0)
+    if ( noZeros.length === 0 ) {
+        return "0"
+    }
+    
+    nums.sort((a, b) => {
+        let testA = `${a}${b}`
+        let testB = `${b}${a}`
+        
+        if (Number(testA) > Number(testB)) {
+            return 1
+        }
+        else {
+            return -1
+        }
+    });
+    
+    return nums.reverse().join('');
+};
